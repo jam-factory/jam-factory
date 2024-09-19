f646b079259a540e2ce50b3ac602e6bbe7d4557b
Tatsuya <a.tatsu0117@gmail.com>
2024-09-20 00:19:50 +0700

    stripeの必須項目を記入

--- app/(routes)/commercial-law/page.jsx
+++ app/(routes)/commercial-law/page.jsx
@@ -32,17 +32,16 @@ export default function CommercialLawPage() {
             <div className={styles.contentInner}>
               <dl className={styles.list}>
                 <div className={styles.item}>
-                  <dt>販売業者名</dt>
+                  <dt>販売業社の名称</dt>
                   <dd>赤尾 起弥</dd>
                 </div>
+
                 <div className={styles.item}>
-                  <dt>代表責任者</dt>
-                  <dd>赤尾 起弥</dd>
-                </div>
-                <div className={styles.item}>
-                  <dt>住所</dt>
+                  <dt>所在地</dt>
                   <dd>
-                    住所については、お問い合わせ先メールアドレスにてご請求をいただければ、遅滞なく開示いたします。
+                    〒530-0001
+                    <br />
+                    大阪府大阪市北区梅田1-1-3大阪駅前第3ビル 29階　1-1-1号室
                   </dd>
                 </div>
                 <div className={styles.item}>
@@ -54,6 +53,10 @@ export default function CommercialLawPage() {
                 <div className={styles.item}>
                   <dt>メールアドレス</dt>
                   <dd>{MAIL_ADDRESS}</dd>
+                </div>
+                <div className={styles.item}>
+                  <dt>運営統括責任者</dt>
+                  <dd>赤尾 起弥</dd>
                 </div>
                 <div className={styles.item}>
                   <dt>ホームページURL</dt>
@@ -68,11 +71,7 @@ export default function CommercialLawPage() {
                   <dd>当サイトに定められたサブスクリプション料金をお支払い頂いている期間</dd>
                 </div>
                 <div className={styles.item}>
-                  <dt>販売価格</dt>
-                  <dd>各サービス詳細ページをご参照ください。</dd>
-                </div>
-                <div className={styles.item}>
-                  <dt>サービス代金以外の必要料金</dt>
+                  <dt>追加手数料等の追加料金</dt>
                   <dd>
                     <ul>
                       <li>消費税（10%）</li>
@@ -83,19 +82,26 @@ export default function CommercialLawPage() {
                   </dd>
                 </div>
                 <div className={styles.item}>
-                  <dt>役務の提供時期</dt>
+                  <dt>交換および返品（返金ポリシー）</dt>
+                  <dd>サービスの性質上、ご購入後のキャンセル・返金はできかねます。</dd>
+                </div>
+                <div className={styles.item}>
+                  <dt>引渡時期</dt>
                   <dd>当方で決済処理確認後即時提供致します。</dd>
                 </div>
                 <div className={styles.item}>
-                  <dt>決済方法と決済時期</dt>
+                  <dt>受付可能な決済手段</dt>
+                  <dd>クレジットカードまたは国内の銀行振込</dd>
+                </div>
+                <div className={styles.item}>
+                  <dt>決済期間</dt>
                   <dd>
-                    Stripe決済（クレジットカード決済の場合はただちに処理されますが、国内の銀行振込の場合は注文から 3
-                    日以内にお振り込みいただく必要があります。）
+                    クレジットカード決済の場合はただちに処理されますが、国内の銀行振込の場合は注文から3日以内にお振り込みいただく必要があります。
                   </dd>
                 </div>
                 <div className={styles.item}>
-                  <dt>契約の申込みの撤回又は解除</dt>
-                  <dd>サービスの性質上、ご購入後のキャンセル・返金はできかねます。</dd>
+                  <dt>販売価格</dt>
+                  <dd>各サービス詳細ページをご参照ください。</dd>
                 </div>
                 <div className={styles.item}>
                   <dt>中途解約について</dt>
